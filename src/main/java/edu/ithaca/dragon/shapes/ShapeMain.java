df1889c138291a4be7f8c903a2b238a9a3d5b337
clapierre22 <clapierre@ithaca.edu>
2024-09-02 15:58:47 -0400

    Added a list of 5 randomly-sized rectangles, with the list then printing out the area and longest line within each Rectangle, in main().

--- src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
+++ src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
@@ -1,10 +1,29 @@
 package edu.ithaca.dragon.shapes;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class ShapeMain {
+
     
     public static void main(String[] args){
         //Make a list of 5 randomly-sized rectangles and print their area and the largest line that can be drawn through them
         //Allow the user to choose one, double the size of that one, and print them all again
         //Use a loop to repeat the process 5 times
+
+        Random randNum = new Random();
+
+        ArrayList<Rectangle> randRecList = new ArrayList<>();
+
+        randRecList.add(new Rectangle(randNum.nextDouble(20), randNum.nextDouble(25)));
+        randRecList.add(new Rectangle(randNum.nextDouble(20), randNum.nextDouble(25)));
+        randRecList.add(new Rectangle(randNum.nextDouble(20), randNum.nextDouble(25)));
+        randRecList.add(new Rectangle(randNum.nextDouble(20), randNum.nextDouble(25)));
+        randRecList.add(new Rectangle(randNum.nextDouble(20), randNum.nextDouble(25)));
+
+        for (Rectangle i : randRecList) {
+            System.out.println("Random Rectangle Area: " + i.calcArea() + " Random Rectangle Longest Line: " + i.longestLineWithin());
+        }
+
     }
 }
