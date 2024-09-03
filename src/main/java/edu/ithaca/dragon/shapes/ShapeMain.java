b8eedb201cce6f6157c3d05b1d2559ec28f9de1e
clapierre22 <clapierre@ithaca.edu>
2024-09-03 13:42:48 -0400

    Accidental extra spacing that was deleted for personal cosmetic reasons

--- src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
+++ src/main/java/edu/ithaca/dragon/shapes/ShapeMain.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 public class ShapeMain {
     
-    
     public static void main(String[] args){
         //Make a list of 5 randomly-sized rectangles and print their area and the largest line that can be drawn through them
         //Allow the user to choose one, double the size of that one, and print them all again
