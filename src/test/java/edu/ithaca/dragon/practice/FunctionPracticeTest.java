bbbc97179ed418682e7b187cec04df39ff7cb372
clapierre22 <clapierre@ithaca.edu>
2024-09-03 13:48:13 -0400

    Added comments to largestOfThreeTest() in FunctionPracticeTest.java

--- src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
+++ src/test/java/edu/ithaca/dragon/practice/FunctionPracticeTest.java
@@ -1,11 +1,8 @@
 package edu.ithaca.dragon.practice;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.Test;
 
 public class FunctionPracticeTest {
@@ -16,10 +13,12 @@ public class FunctionPracticeTest {
         assertEquals(5, FunctionPractice.largestOfThree(5, 4, 2));
         assertEquals(5, FunctionPractice.largestOfThree(4, 2, 5));
         assertEquals(5, FunctionPractice.largestOfThree(5, 5, 5));
+        // Above test checks function to ensure that it executes in the manner intedned in the docstring
 
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(-1, -1, -1));
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(5, -1, 3));
         assertThrows(IllegalArgumentException.class, () -> FunctionPractice.largestOfThree(5, 3, -1));
+        // Above test checks functon to make sure that it resonds to illegal inputs in the manner intended in the docstring
     }
 
     @Test
